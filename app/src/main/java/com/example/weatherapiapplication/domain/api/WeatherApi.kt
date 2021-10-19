cc6faa94c29a31725893172e7e7ce8bcb5c43c86
Aleks <kuriatae@mail.ru>
2021-10-19 20:53:14 +0700

    Apikey перенесен в отдельный файл, в gitignore не добавлял, чтобы была возможность проверки

--- app/src/main/java/com/example/weatherapiapplication/domain/api/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapiapplication/domain/api/WeatherApi.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapiapplication.domain.api
 
 
+import com.example.weatherapiapplication.BuildConfig
 import com.example.weatherapiapplication.domain.weatherModel.CityWeatherModel
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
@@ -11,7 +12,7 @@ interface WeatherApi {
     @GET("2.5/weather?")
     fun getWeatherCity(
         @Query("q") cityName: String,
-        @Query("appid") apiKey : String = "46ecefd21bc6e54164835663ebe5ac6f"
+        @Query("appid") apiKey : String = BuildConfig.WEATHER_API_KEY
     ): Single<CityWeatherModel>
 
 
